28497b63caa207fbdad5093aaf36882008ac577a
DarienZzZz <146400531+DarienZzZz@users.noreply.github.com>
2023-09-29 15:06:59 -0600

    Programa 2

---
+++ Unidad2/Programa2.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+namespace Programa2;
+
+ internal class suma
+{
+    static void Main(string[] args)
+    {
+        int num1;
+        int num2;
+        int suma;
+
+        Console.WriteLine("Ingrese el Numero 1:");
+        num1 = Int32.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese el Numero 2:");
+        num2 = Int32.Parse (Console.ReadLine());
+
+        suma= num1 + num2;
+        Console.WriteLine("La suma es:" + suma);
+
+    }
+}
