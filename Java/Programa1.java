e9f5c8187a6c4d0523b277b5bcbde7b3dfac4004
DarienZzZz <146400531+DarienZzZz@users.noreply.github.com>
2023-10-03 19:58:06 -0600

    Add files via upload

---
+++ Java/Programa1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programa1;
+import java.util.Scanner;
+
+public class Programa1 
+{
+    public static void main(String[] args) 
+    {
+       
+       String nombre = "Darien Zavala ";
+       
+       System.out.println(nombre);
+        
+    }
+}
