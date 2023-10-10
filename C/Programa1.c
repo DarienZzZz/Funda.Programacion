43c25e7afa14c0395d5e86688fff4f7636ad27a1
DarienZzZz <146400531+DarienZzZz@users.noreply.github.com>
2023-10-09 19:47:32 -0600

    Add files via upload

---
+++ C/Programa1.c
@@ -0,0 +1,14 @@
+int main()
+{
+    int a, b, suma;
+    
+    printf("Escribe el valor de A:");
+    scanf ("%d", &a);
+    printf("Escribe el valor de B:");
+    scanf ("%d", &b);
+
+    suma = a + b;
+    
+    printf("Redsultados: %d", suma);
+    return 0;
+}
