a4856c9aab505f8580c1cd6723721a288fd99dbd
DarienZzZz <146400531+DarienZzZz@users.noreply.github.com>
2023-11-15 19:32:55 -0600

    Add files via upload

---
+++ Unidad4/Programa11U4.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programa11u4;
+
+/**
+ *
+ * @author LaboratorioLTI2
+ */
+public class Programa11U4 {
+
+    public static void main(String[] args) {
+        /*int[][]num = new int[3][3];
+        
+        num[0][0]=5;
+        num[0][1]=7;
+        num[0][2]=9;
+        num[1][0]=8;
+        num[1][1]=9;
+        num[1][2]=5;
+        num[2][0]=6;
+        num[2][1]=7;
+        num[2][2]=9;*/
+        int[][]num={{5,7,8},{10,5,6},{10,10,5}};
+        
+        System.out.print(num[0][0]+ " ");
+        System.out.print(num[0][1]+ " ");
+        System.out.println(num[0][2]);
+        System.out.print(num[1][0]+ " ");
+        System.out.print(num[1][1]+ " ");
+        System.out.println(num[1][2]);
+        System.out.print(num[2][0]+ " ");
+        System.out.print(num[2][1]+ " ");
+        System.out.print(num[2][2]);
+    }
+}
