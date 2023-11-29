4932b3092e18955491b1af4a9b895c8d675203b9
DarienZzZz <146400531+DarienZzZz@users.noreply.github.com>
2023-11-28 19:55:54 -0600

    Add files via upload

---
+++ Unidad5/Programa1U5.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programa1u5;
+
+/**                                     
+ *
+ * @author LaboratorioLTI2
+ */
+public class Programa1U5 {
+
+    public static void main(String[] args) {
+        System.out.println(suma(7,8));
+        
+        
+    }
+    //Metodo
+   public static int suma(int a, int b)
+   {
+       int r=a+b;
+       return r;
+   }
+}
