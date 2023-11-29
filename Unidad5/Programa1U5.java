b2242315dc588cc05f833d45352bd2c969525eaf
DarienZzZz <146400531+DarienZzZz@users.noreply.github.com>
2023-11-29 00:15:28 -0600

    Add files via upload

--- Unidad5/Programa1U5.java
+++ Unidad5/Programa1U5.java
@@ -1,24 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+/******************************************************************************
 
-package com.mycompany.programa1u5;
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
 
-/**                                     
- *
- * @author LaboratorioLTI2
- */
-public class Programa1U5 {
+*******************************************************************************/
 
-    public static void main(String[] args) {
-        System.out.println(suma(7,8));
-        
-        
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println(suma(7,8));
+        System.out.println(res(7,8));
+        System.out.println(mul(7,8));
+        System.out.println(div(7,8));
     }
     //Metodo
+   public static int res(int a, int b)
+   {
+       int r=a-b;
+       return r;
+   }
+   public static int mul(int a, int b)
+   {
+       int r=a*b;
+       return r;
+   }
    public static int suma(int a, int b)
    {
        int r=a+b;
+       return r;
+   }
+   public static double div(double a, double b)
+   {
+       double r=a/b;
        return r;
    }
 }
